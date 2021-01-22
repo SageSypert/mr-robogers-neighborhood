22d29c4365af8884f4c2da2455d94a411f9ac526
Sage Sypert <sage.sypert@gmail.com>
2021-01-22 15:24:44 -0800

    add more css styling, cleaning up scripts, ready to push

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,3 @@
-// Describe: userInputArray(numbers)
-
-// Test: If any number inputted by user contains a 1 it is returned with the word "Beep!"
-// Expect: userNumber.push(beep)
-
-// Test: If any number inputted by user contains a 2 it is returned with word "Boop!"
-// Expect:  userNumber.push(boop)
-
-// Test: If any number inputted by user contains a 3 it is returned with word "Won't you be my neighbor?"
-// Expect:  userNumber.push(neighbor)
-
-// Test: Program will recognize when a number contains 1,2, or 3 and return with a string of numbers replacing 1,2, or 3 with "beep, "boop", or "Won't you be my neighbor?"
-// Expect:  return userNumber
-
-// Test: Program will recognize when a number contains 1,2, or 3 and return with a string of numbers replacing 1,2, or 3 with "beep, "boop", or "Won't you be my neighbor?". Program will still include range of other numbers not matching those conditions in the string.
-// Expect:  return userNumber
-
 // User Interface Logic: 
 
 $(document).ready(function() {
@@ -23,7 +6,9 @@ $(document).ready(function() {
     
     const number = ($("input#number").val());
     const userResult = userInputArray(number);
+    
     $("#resultNumbers").text(userResult);
+  
   });
 });
 
