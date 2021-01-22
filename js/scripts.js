f05179ab668e38e90b288d295429e09d3a9d7c53
Sage Sypert <sage.sypert@gmail.com>
2021-01-22 12:00:54 -0800

    add function with if statement and console log to test

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Describe: numberInput(number)
-// Test: If any number contains a 1 it is replaced with "Beep!"
+// Test: If any number contains a 1 it is returned with the word "Beep!"
 // Expect: 
 
 // let 1 === true
@@ -7,14 +7,18 @@
 
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
+    event.preventDefault();
 
-    let resultArray = [];
 
-
-    function numberInput(number) {
-      if (number )
+  function number(number) {
+    const userInput = document.getElementById("number");
+    if (userInput.includes(number(1))) {
+      return number + "Beep!"
     }
+    
 
+    }
+  });
+});
 
-  }
-}
+console.log(number("10, Beep!"))
