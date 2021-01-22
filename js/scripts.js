d76c52d51c08824c9bd40476d24b274b788c6966
Sage Sypert <sage.sypert@gmail.com>
2021-01-22 12:16:10 -0800

    separate business and UI logic, add const variables

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,16 @@
 // Test: If any number contains a 1 it is returned with the word "Beep!"
 // Expect: 
 
-// let 1 === true
-
+// User Interface Logic: 
 
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
-
-
-  function number(number) {
-    const userInput = document.getElementById("number");
-    if (userInput.includes(number(1))) {
-      return number + "Beep!"
-    }
-    
-
-    }
   });
 });
 
-console.log(number("10, Beep!"))
+// Business Logic:
+
+const beep = "beep!"
+const boop = "boop!"
+const neighbor = "Won't you be my neighbor?"
--- notes.md
+++ notes.md
@@ -1,3 +1,5 @@
 2021-01-22 10:56:44 - Researching arrays and loops in this week's lessons to deterine next steps in scripts.js. Unsure of which loop to use or how to set up function.
 
 2021-01-22 11:37:07 - Still researching different looping lessons and trying to see what works in console. 
+
+2021-01-22 12:12:39 - Deleting scripts and starting over :(
