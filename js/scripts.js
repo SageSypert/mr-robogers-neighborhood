fef4c460d2b879900de6278d8b00dedf20dbb877
Sage Sypert <sage.sypert@gmail.com>
2021-01-22 10:17:26 -0800

    begin first TDD notes on scripts page, html styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
+// Describe:
+// Test: Number that contains a 1 is replaced with "Beep!"
+// Expect: 
+
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
+
+    
+
 
   }
 }
