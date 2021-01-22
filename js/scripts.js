c3ff243be2da9933e0cb43224b02d773f9895a09
Sage Sypert <sage.sypert@gmail.com>
2021-01-22 10:52:58 -0800

    add different arrays and outlines in scripts, researching loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
-// Describe:
-// Test: Number that contains a 1 is replaced with "Beep!"
+// Describe: numberInput(number)
+// Test: If any number contains a 1 it is replaced with "Beep!"
 // Expect: 
+
+// let 1 === true
+
 
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
 
-    
+    let resultArray = [];
+
+
+    function numberInput(number) {
+      if (number )
+    }
 
 
   }
